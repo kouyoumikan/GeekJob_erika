168dcc5cb7d4b9918705f2751a321e6695cbcc0c
GeekJob_erika <kouyoumikan136@gmail.com>
2018-05-25 16:40:44 +0900

    java12-5 標準クラス課題提出します

---
+++ Java/CampChallenge_java/CampChallenge_java/015.標準クラス/NewClass6.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author guest1Day
+ */
+
+//自分の氏名について，バイト数と文字数を取得してください。また，それぞれの値を画面に表示
+public class NewClass6 {
+     public static void main(String[] args) {
+        String word = "小林　英里香";
+
+        // 文字列の長さを表示 -- 6
+        System.out.println(word.length());
+    }
+}
