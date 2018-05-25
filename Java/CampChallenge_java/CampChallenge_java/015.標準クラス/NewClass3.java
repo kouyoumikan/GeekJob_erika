ca3763265b22b4f121fa20202c60e85c093ee4ec
GeekJob_erika <kouyoumikan136@gmail.com>
2018-05-25 16:40:08 +0900

    java12-2 標準クラス課題提出します

---
+++ Java/CampChallenge_java/CampChallenge_java/015.標準クラス/NewClass3.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.Calendar;
+/**
+ *
+ * @author guest1Day
+ */
+
+//現在の日時を「1988年5月21日 1時23分45秒」といった形式で表現
+public class NewClass3 {
+     public static void main(String[] args) {
+        // カレンダーインスタンスの作成
+        Calendar c = Calendar.getInstance();
+        // 2017年8月12日 18時10分33秒
+        c.set(1988, 5, 21, 1, 23, 45);
+
+        // 年取得
+        System.out.print(c.get(Calendar.YEAR));
+    }
+
+}
