1f187e8c775650972608e683b1052f0dfee68871
GeekJob_erika <kouyoumikan136@gmail.com>
2018-07-27 13:20:04 +0900

    php4　課題提出します

---
+++ PHP/CampChallenge_PHP/CampChallenge_PHP/004.文字列/文字の連結表示.php
@@ -0,0 +1,9 @@
+<?php
+        // 「groove」「-」「gear」の３つの文字列を連結して表示してください
+
+          const TEXT = "groove";
+          const KIGOU = "-";
+          $text = "gear";
+
+          echo TEXT. KIGOU. $text;
+        ?>
