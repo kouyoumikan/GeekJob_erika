e97130968072050207d5e55cda3d69e042dc96d2
GeekJob_erika <kouyoumikan136@gmail.com>
2018-07-27 13:19:38 +0900

    php3　課題提出します

---
+++ PHP/CampChallenge_PHP/CampChallenge_PHP/003.四則演算/変数の宣言と表示（四則演算）.php
@@ -0,0 +1,19 @@
+<?php
+        // 定数と変数を宣言し、それぞれに数値を入れて四則演算を行い、四則演算の結果をそれぞれ表示
+
+          const NAMBER = 200;
+          $num = 5;
+
+          $amari = NAMBER % $num;
+          $tashi = NAMBER + $num;
+          $hiki = NAMBER - $num;
+          $kake = NAMBER * $num;
+          $wari = NAMBER / $num;
+
+          echo $amari;
+          echo $tashi;
+          echo $hiki;
+          echo $kake;
+          echo $wari;
+
+        ?>
