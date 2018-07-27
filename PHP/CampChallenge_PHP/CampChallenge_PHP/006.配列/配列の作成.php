a74ba086075624b7104aad7f71ba6c3054450bde
GeekJob_erika <kouyoumikan136@gmail.com>
2018-07-27 13:21:10 +0900

    php6　課題提出します

---
+++ PHP/CampChallenge_PHP/CampChallenge_PHP/006.配列/配列の作成.php
@@ -0,0 +1,15 @@
+<?php
+        // 以下の順番で、要素が格納された配列を作成してください。
+       // "10", "100", "soeda", "hayashi", "-20", "118", "END"
+
+          $KEY = array("10", "100", "soeda", "hayashi", "-20", "118", "END");
+
+          echo $KEY[0];
+          echo $KEY[1];
+          echo $KEY[2];
+          echo $KEY[3];
+          echo $KEY[4];
+          echo $KEY[5];
+          echo $KEY[6];
+
+        ?>
