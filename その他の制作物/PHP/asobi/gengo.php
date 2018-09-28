822730787e96fa8b7a79db06b9440509478b57db
GeekJob_erika <kouyoumikan136@gmail.com>
2018-09-28 11:42:07 +0900

    西暦を和暦に

---
+++ その他の制作物/PHP/asobi/gengo.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+    <title>西暦から元号を知る</title>
+</head>
+<body>
+
+  <?php
+
+//    requie_once('../common/common.php'); // commonの中にある関数を呼び出す
+
+    $seireki = $_POST['seireki']; // 関数で西暦に応じた元号を返す
+
+    $wareki = gengo($seireki); // その元号を表示
+    print $wareki; // この先は関数なので、プログラムの終了
+    print '<br>';
+
+     function gengo($seireki){
+       if(1868 <= $seireki && $seireki <= 1911){
+         $gengo = '明治';
+       }
+       if(1912 <= $seireki && $seireki <= 1925){
+         $gengo = '大正';
+       }
+       if(1926 <= $seireki && $seireki <= 1988){
+         $gengo = '昭和';
+       }
+       if(1989 <= $seireki && $seireki <= 2018){
+         $gengo = '平成';
+       }
+       if(2019 <= $seireki){
+         $gengo = '';
+       }
+       return($gengo);
+     }
+
+   ?>
+
+</body>
+</html>
