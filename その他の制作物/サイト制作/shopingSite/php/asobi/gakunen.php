a1d5f1f838442c73b96be22ce19a3b7156af0208
GeekJob_erika <kouyoumikan136@gmail.com>
2018-09-28 11:40:50 +0900

    サイト制作　更新

--- その他の制作物/サイト制作/shopingSite/php/asobi/gakunen.php
+++ その他の制作物/サイト制作/shopingSite/php/asobi/gakunen.php
@@ -13,34 +13,34 @@
     switch ($gakunen) {
       case '1':
         // code...
-        $kosya = 'あなたの校舎は南校舎です。';
+        $kousha = 'あなたの校舎は南校舎です。';
         $bukatsu = '部活動にはスポーツ系と文化系があります。';
         $mokuhyou = 'まずは学校に慣れましょう。';
         break;
 
       case '2':
         // code...
-        $kosya = 'あなたの校舎時は西校舎です。';
+        $kousha = 'あなたの校舎時は西校舎です。';
         $bukatsu = '学園祭目指して全力で取り組みましょう。';
         $mokuhyou = '今しか出来ないことを見つけましょう。';
         break;
 
       case '3':
         // code...
-        $kosya = 'あなたの校舎は東校舎です。';
+        $kousha = 'あなたの校舎は東校舎です。';
         $bukatsu = '受験に就職に忙しくなります。　後輩へ譲っていきましょう。';
         $mokuhyou = '早く卒業しましょう。';
         break;
 
       default:
         // code...
-        $kosya = 'あなたの校舎は三年生と同じです。';
+        $kousha = 'あなたの校舎は三年生と同じです。';
         $bukatsu = '部活動はありません。';
         $mokuhyou = '早く卒業しましょう。';
         break;
     }
 
-    print '校舎: ' .$kousya. ' <br>';
+    print '校舎: ' .$kousha. ' <br>';
     print '部活: ' .$bukatsu. ' <br>';
     print '目標: ' .$mokuhyou. ' <br>';
 
